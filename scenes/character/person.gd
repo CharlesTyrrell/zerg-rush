44dd0feeb17c643c900b80af37b380b0005647b0
CharlesTyrrell <kellentyrrell@yahoo.com>
2022-10-19 14:02:25 -0800

    tweaked a few things
    -added entities, called tiles
    -added animations for tiles
    -added automove for character player
    -nodes now clip to grid

--- scenes/character/person.gd
+++ scenes/character/person.gd
@@ -1,25 +1,76 @@
 extends Node2D
 
+enum {
+	U, R, D, L
+}
 
+const move_time_step = 60
+const move_time_start = move_time_step*2
+const move_time_iter = move_time_step 
 
 var snap = load("res://global/StaticValues.gd").CELL_LENGTH
+var move_dir : int
+var timer : int
 
-
-func _ready():
-	pass
+func _process(_delta):
+	timer += 1
 	
 	
+	
+	if Input.is_action_pressed("ui_right"):
+		if Input.is_action_just_pressed("ui_right"):
+			position.x += snap
+			$Sprite.flip_h = false
+			
+			timer = 1
+		if(timer > move_time_start && timer % move_time_iter == 0):
+			position.x += snap
+	
+	if Input.is_action_pressed("ui_left"):
+		if Input.is_action_just_pressed("ui_left"):
+			position.x -= snap
+			$Sprite.flip_h = false
+			
+			timer = 1
+		
+		if(timer > move_time_start && timer % move_time_iter == 0):
+			position.x -= snap
+	
+	if Input.is_action_pressed("ui_down"):
+		if Input.is_action_just_pressed("ui_down"):
+			position.y += snap
+			$Sprite.flip_h = false
+			
+			timer = 1
+		
+		if(timer > move_time_start && timer % move_time_iter == 0):
+			
+			position.y += snap
+	
+	if Input.is_action_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_up"):
+			position.y -= snap
+			$Sprite.flip_h = false
+			
+			timer = 1
+		
+		
+		if(timer > move_time_start && timer % move_time_iter == 0):
+			
+			position.y -= snap
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_right"):
-		position.x += snap
-		$Sprite.flip_h = false
+
+
+
 	if Input.is_action_just_pressed("ui_left"):
+		move_dir = L
 		position.x -= snap
 		$Sprite.flip_h = true
 	if Input.is_action_just_pressed("ui_down"):
+		move_dir = D
 		position.y += snap
 	if Input.is_action_just_pressed("ui_up"):
+		move_dir = U
 		position.y -= snap
 		
 	
---
+++ scenes/tiles/tile.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+enum{
+	U, R, D, L #UP, RIGHY, LEFT, DOWN
+}
+
+var snap = load("res://global/StaticValues.gd").CELL_LENGTH
+
+
+func move(dir : int):
+	match dir:
+		R:
+			position.x += snap
+			$Sprite.flip_h = false
+		L:
+			position.x -= snap
+			$Sprite.flip_h = true
+		D:
+			position.y += snap
+		U:
+			position.y -= snap
+		
